52c9b23d5743fac91fbb3c591d1f1a1b6aee00c0
maltetoelle <malte.toelle@gmail.com>
2020-11-05 14:27:34 +0100

    linear blog post

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,15 @@ $(document).ready(function () {
 
     }
 });
+
+// $("script[type='math/tex']").replaceWith(
+//   function(){
+//     var tex = $(this).text();
+//     return katex.renderToString(tex, {displayMode: false});
+// });
+//
+// $("script[type='math/tex; mode=display']").replaceWith(
+//   function(){
+//     var tex = $(this).text();
+//     return katex.renderToString(tex.replace(/%.*/g, ''), {displayMode: true});
+// });
