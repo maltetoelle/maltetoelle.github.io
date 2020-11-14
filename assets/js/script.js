146346a2b55baf7d169a0ff160c32c7fd38be28c
maltetoelle <malte.toelle@gmail.com>
2020-11-14 12:39:16 +0100

    js cookie

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,12 @@ $("a.bibtexs").click(function(evt) {
  }
 })
 
-var cookiesClicked = false
-var cookies = false
+// var cookiesClicked = false
+// var cookies = false
 
 function getCookieAgreement() {
-  // Cookies.set('name', 'value')
-  if (cookiesClicked == false) {
+  var cookiesClicked = Cookies.get('cookiesClicked')
+  if (typeof cookiesClicked == 'undefined') {
     $(".cookie-warning").css("bottom", $(".footer").height() + 30);
     $(".cookie-blocker").css({
      "position": "absolute",
@@ -35,13 +35,17 @@ function getCookieAgreement() {
 }
 
 $("#cookie-yes").click(() => {
- cookies = true;
- cookiesClicked = true;
+ Cookies.set('cookies', 'true', { expires: 30 })
+ Cookies.set('cookiesClicked', 'true', { expires: 30 })
+ // cookies = true;
+ // cookiesClicked = true;
  getCookieAgreement();
 })
 
 $("#cookie-no").click(() => {
- cookiesClicked = true;
+ // cookiesClicked = true;
+ Cookies.set('cookies', 'false', { expires: 30 })
+ Cookies.set('cookiesClicked', 'true', { expires: 30 })
  getCookieAgreement();
 })
 
@@ -49,27 +53,31 @@ $(document).ready(function () {
 
     getCookieAgreement();
     // Read the cookie and if it's defined scroll to id
-    var scroll = $.cookie('scroll');
+    // var scroll = $.cookie('scroll');
+    var scroll = Cookies.get('scroll')
+    console.log(scroll)
     if(scroll){
         scrollToID(scroll, 1000);
-        $.removeCookie('scroll');
+        // Cookies.remove('scroll')
     }
 
     // Handle event onclick, setting the cookie when the href != #
     $('.nav-click').find('a').click(function (e) {
         var href = $(this).attr('href');
+        const validHrefs = ['/', '#intro', '#pubs', '#posts']
         if (href == "/contact") {
+
          return False
         } else {
              e.preventDefault();
-             console.log('here')
-             // var id = $(this).attr('id')
-
-             console.log(href)
-             if(window.location.pathname == "/contact"){
-                 $.cookie('scroll', href);
-                 scroll = 'have it'
+             if(validHrefs.indexOf(window.location.pathname) == -1){
+                 // $.cookie('scroll', href);
+                 if (Cookies.get('cookies') == 'true') {
+                  Cookies.set('scoll', href)
+                 }
+                 // scroll = 'have it'
                  window.location.href = window.location.origin;
+                 // scrollToID(href, 500);
              }else{
                  scrollToID(href, 500);
              }
@@ -78,6 +86,7 @@ $(document).ready(function () {
 
     // scrollToID function
     function scrollToID(id, speed) {
+        console.log(id)
         var $href = $(id).attr('href');
         var $anchor = $(id).offset();
         if ($href == "#intro") {
@@ -87,7 +96,7 @@ $(document).ready(function () {
         }
         targetOffset = $anchor.top - offset
         $('html,body').animate({ scrollTop: targetOffset }, speed);
-
+        // Cookies.remove('scroll')
     }
 
 });
